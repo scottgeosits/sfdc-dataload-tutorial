202862002fd692f39e621a247844a6efb8961494
Geosits, Scott J <scottgeosits@gmail.com>
2015-04-24 20:21:40 -0400

    Add missing bin directory.

---
+++ bin/encrypt.bat
@@ -0,0 +1,3 @@
+@echo off
+
+java -cp ../lib/dataloader-27.0.1-uber.jar com.salesforce.dataloader.security.EncryptionUtil %*
---
+++ bin/encrypt.sh
@@ -0,0 +1,1 @@
+java -cp ../lib/dataloader-29.0.0-uber.jar com.salesforce.dataloader.security.EncryptionUtil $*
